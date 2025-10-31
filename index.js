f54468a7902a7402f818d170129a919f87b1ce68
LilyClausA <lilcla1230@students.maquoketaschools.org>
2025-10-31 12:33:48 -0500

    Add introduction string to index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let introduction = 'bleep bloop,\n' +
+  "\tYADAYADA \'HEHEHE\' SPOOKY SCARY.\n" +
+  'SKELOTENS \"send\" SHIVERS DOWN THY SPINE.\n\n' +
+  'Andrew Gold \\'
+document.getElementById('result').innerHTML = introduction;
